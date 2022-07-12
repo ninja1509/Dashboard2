a60639eac1b93fb6a52c61dcf24e8c815c24f07b
ninja1509 <63071772+ninja1509@users.noreply.github.com>
2022-07-12 22:32:44 +0530

    Add files via upload

---
+++ screens/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+import NavBar from '../component/Navbar';
+function Login() {
+    return (
+        <div>
+            <NavBar/>
+            
+        </div>
+    );
+}
+
+export default Login;
